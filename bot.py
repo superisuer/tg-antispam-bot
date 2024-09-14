d1ec466f6f44d15eb4f81834446b9787832f057e
Den <superisuer@proton.me>
2024-09-14 12:41:34 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -8,9 +8,15 @@ token='' # Token from BotFather
 blocked_urls = ["t.me","tiktok.ru","iplogger.com"]     # Blocked URLs in message.text
 bot_can_send_messages = True                           # Determines whether the bot can send messages to chat
 max_messages_in_list = 5                               # How much messages can write in bot memory
+bot_can_block_admin_messages = True                    # Admin immunity to bot
 contact_username = "huekx"                             # Username for contact
+max_symbols_in_message = 2000                          # Maximum number of allowed characters
 # ----------------------------------------------------
-lastsms = [] 
+lastsms = []
+if bot_can_block_admin_messages:
+    memberstatus = ["administator", "owner", "member"]
+else:
+    memberstatus = ["member"]
 bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -19,28 +25,29 @@ def start_message(message):
 def check_message(message):
     threading.Thread(target=message_reply,args=[message]).start()
 def message_reply(message):
-    if len(message.text) > 1000:
-        try:
-            bot.delete_message(message_id=message.id,chat_id=message.chat.id)
-        except Exception as e:
-            print(e)
-        else:
-            if bot_can_send_messages:
-                bot_message = bot.send_message(message.chat.id, f"The suspicious message has been deleted. If your message was deleted by mistake, please contact @{contact_username}.")
-                time.sleep(3)
-                bot.delete_message(message_id=bot_message.id,chat_id=bot_message.chat.id)
-    if len(lastsms) > max_messages_in_list:
-        lastsms.clear()
+    if bot.get_chat_member(user_id=message.from_user.id,chat_id=message.chat.id).status in memberstatus:
+        if len(message.text) > max_symbols_in_message:
+            try:
+                bot.delete_message(message_id=message.id,chat_id=message.chat.id)
+            except Exception as e:
+                print(e)
+            else:
+                if bot_can_send_messages:
+                    bot_message = bot.send_message(message.chat.id, f"The suspicious message has been deleted. If your message was deleted by mistake, please contact @{contact_username}.")
+                    time.sleep(3)
+                    bot.delete_message(message_id=bot_message.id,chat_id=bot_message.chat.id)
+        if len(lastsms) > max_messages_in_list:
+            lastsms.clear()
     
-    if message.text in lastsms:
-        try:
-            bot.delete_message(message_id=message.id,chat_id=message.chat.id)
-        except Exception as e:
-            print(e)
-        else:
-            if bot_can_send_messages:
-                bot_message = bot.send_message(message.chat.id, f"The suspicious message has been deleted. If your message was deleted by mistake, please contact @{contact_username}.")
-                time.sleep(3)
+        if message.text in lastsms:
+            try:
+                bot.delete_message(message_id=message.id,chat_id=message.chat.id)
+            except Exception as e:
+                print(e)
+            else:
+                if bot_can_send_messages:
+                    bot_message = bot.send_message(message.chat.id, f"The suspicious message has been deleted. If your message was deleted by mistake, please contact @{contact_username}.")
+                    time.sleep(3)
                 bot.delete_message(message_id=bot_message.id,chat_id=bot_message.chat.id)
     for i in range(len(blocked_urls)):
         if blocked_urls[i] in message.text:
@@ -53,6 +60,6 @@ def message_reply(message):
                     bot_message = bot.send_message(message.chat.id, f"The suspicious message has been deleted. If your message was deleted by mistake, please contact @{contact_username}.")
                     time.sleep(3)
                     bot.delete_message(message_id=bot_message.id,chat_id=bot_message.chat.id)
-    print(f"{message.text} - from @{message.from_user.username}, {message.chat.title}")
-    lastsms.append(message.text)
+        print(f"{message.text} - from @{message.from_user.username}, {message.chat.title}")
+        lastsms.append(message.text)
 bot.infinity_polling(none_stop = True)
